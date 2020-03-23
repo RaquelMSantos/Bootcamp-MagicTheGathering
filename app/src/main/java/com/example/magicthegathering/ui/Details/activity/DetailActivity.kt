5905f317ef125e6a619b41938cf6b462d3f87686
Raquel Santos <raquelmsantos.ol@gmail.com>
2020-03-23 12:28:48 -0300

    remocao de linha

--- app/src/main/java/com/example/magicthegathering/ui/Details/activity/DetailActivity.kt
+++ app/src/main/java/com/example/magicthegathering/ui/Details/activity/DetailActivity.kt
@@ -6,6 +6,8 @@ import com.example.magicthegathering.R
 
 class DetailActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
